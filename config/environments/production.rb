ed1b7a074b985e9bf184ff0b309748b77cfb01ae
Thiago Correa <thiagobocorrea@gmail.com>
2016-02-05 15:02:38 -0200

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
 
   # Action Cable endpoint configuration
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://damp-woodland-76999.herokuapp.com' ]
   config.action_cable.disable_request_forgery_protection = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
